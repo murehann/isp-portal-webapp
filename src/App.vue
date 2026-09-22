d12c079ed6cc448d8acd729164b6bd5a5afc5f9b
Muhammad Rehan <murehan@nisum.com>
2026-09-22 19:45:51 +0500

    (app) - install vue-sonnet and setup Toaster

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Toaster } from 'vue-sonner'
+import 'vue-sonner/style.css'
+</script>
 
 <template>
   <RouterView />
+  <Toaster
+    rich-colors
+    :expand="true"
+    closeButton
+    position="top-right"
+    closeButtonPosition="top-left"
+  />
 </template>
 
 <style scoped></style>
